720d6e22d05b3d35b0017e809ce7dfb037481568
nottimnik <timnikofficial@gmail.com>
2022-04-10 18:00:07 +0000

    :lipstick: Improved the design of the 404 page

--- src/pages/NotfoundPage.jsx
+++ src/pages/NotfoundPage.jsx
@@ -1,11 +1,20 @@
-import { Heading } from '@chakra-ui/react'
-import React from 'react'
-import { Layout } from '../components/Layout'
+import { Heading, Flex } from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Layout } from "../components/Layout";
 
 export default function NotfoundPage() {
   return (
     <Layout>
-      <Heading>Not found page</Heading>
+      <Flex flexDirection="column" justifyContent="center" textAlign="center">
+        <Heading>We couldn't find this page :(</Heading>
+
+        <Flex justifyContent="center">
+          <img src="/images/404.png" width="400px" />
+        </Flex>
+
+        <Link to="/">Go to Home</Link>
+      </Flex>
     </Layout>
-  )
+  );
 }
