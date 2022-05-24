e5e1cf74c6735afb0385335f831ab566470ffb38
nottimnik <timnikofficial@gmail.com>
2022-05-24 18:54:43 +0000

    Removed the unnecesarry information

--- src/markets/components/Exchanges/ExchangeNavbar.jsx
+++ src/markets/components/Exchanges/ExchangeNavbar.jsx
@@ -27,44 +27,6 @@ export default function ExchangeNavbar() {
           </Link>
         </TabList>
       </Tabs>
-
-      <br />
-
-      <Flex>
-        <Stat>
-          <StatLabel fontSize="18px" fontWeight="700">
-            Coins
-          </StatLabel>
-          <StatNumber>3868</StatNumber>
-          <StatHelpText>Tracked</StatHelpText>
-        </Stat>
-
-        <Stat>
-          <StatLabel fontSize="18px" fontWeight="700">
-            Exchanges
-          </StatLabel>
-          <StatNumber>250</StatNumber>
-          <StatHelpText>Tracked</StatHelpText>
-        </Stat>
-
-        <Stat>
-          <StatLabel fontSize="18px" fontWeight="700">
-            Indexes
-          </StatLabel>
-          <StatNumber>29</StatNumber>
-          <StatHelpText>Tracked</StatHelpText>
-        </Stat>
-
-        <Hide breakpoint="(max-width: 550px)">
-          <Stat>
-            <StatLabel fontSize="18px" fontWeight="700">
-              Categories
-            </StatLabel>
-            <StatNumber>75</StatNumber>
-            <StatHelpText>Available</StatHelpText>
-          </Stat>
-        </Hide>
-      </Flex>
     </>
   );
 }
