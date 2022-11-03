f3cb77c994085e004c5a17271288b2f209aa72f7
timnik <72356939+nottimnik@users.noreply.github.com>
2022-11-03 16:37:21 -0700

    Changed the hover effect

--- src/components/Navlink.jsx
+++ src/components/Navlink.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink as Link, useLocation } from "react-router-dom";
 import { Button } from "@chakra-ui/react";
 
 export default function Navlink({ to, name, activated, ...rest }) {
   const location = useLocation();
+  const [show, setShow] = useState(false);
 
-  const isActive = location.pathname === to;
+  const showButton = (e) => {
+    setShow(true);
+  };
+  const hideButton = (e) => {
+    setShow(false);
+  };
 
   return (
-    <Button
-      variant={activated ? "solid" : "ghost"}
-      colorScheme={activated ? "green" : ""}
-      {...rest}
-    >
-      {name}
-    </Button>
+    <div onMouseEnter={showButton} onMouseLeave={hideButton}>
+      {show || activated ? (
+        <Button
+          variant="solid"
+          colorScheme="green"
+          {...rest}
+          padding="10px"
+          marginRight="10px"
+          height="35px"
+        >
+          {name}
+        </Button>
+      ) : (
+        <Button
+          variant="ghost"
+          {...rest}
+          padding="10px"
+          marginRight="10px"
+          height="35px"
+        >
+          {name}
+        </Button>
+      )}
+    </div>
   );
 }
